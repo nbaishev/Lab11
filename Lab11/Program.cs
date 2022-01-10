ddd649d1ed6626b384d9bba8dfc55e4a24c9fbe6
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-01-10 16:14:08 +0900

    Add project files.

---
+++ Lab11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation eq = new Equation { k = 1, b = 10 };
+            eq.Root();
+            Console.ReadKey();
+        }
+
+        struct Equation
+        {
+            public float k;
+            public float b;
+
+            public void Root()
+            {
+               Console.WriteLine("x={0:F2}", -b / k);
+            }
+        }
+    }
+}
